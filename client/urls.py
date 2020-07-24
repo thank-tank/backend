6964f810c187b86a1682f15703a1c85ec4ae1509
Benjamin Kim <benkim96@gmail.com>
2020-07-24 15:28:56 -0700

    start post endpoint for device

--- client/urls.py
+++ client/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='index'),
-        ]
+    path('', views.index, name='index'),
+    path('<int:device_id>/', views.getData, name='device'),
+]

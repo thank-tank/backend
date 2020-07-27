255154153aea0425a06b7b60b7d310bc6bcf99a6
Benjamin Kim <benkim96@gmail.com>
2020-07-27 13:06:53 -0700

    iot client post endpoint configured

--- iotClient/views.py
+++ iotClient/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+import json
+
+def index(request):
+    print("Client Index")
+    return HttpResponse("Client Index")
+
+@csrf_exempt
+def submitData(request):
+    if request.method == "GET":
+        return JsonResponse({'data': [1, 2, 3]})
+    elif request.method == "POST":
+        print("successful post method")
+        print(request.body)
+        json_data = json.loads(request.body)
+        print(json_data)
+        print(json_data['sensor'])
+        print(json_data['data'])
+        return HttpResponse("success")

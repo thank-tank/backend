ac4604c1f762c8ecb43bed348cee23c95a6af1d2
Ubuntu <ubuntu@ip-172-31-9-33.us-west-1.compute.internal>
2020-08-02 01:40:38 +0000

    updates

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.makePost, name='create_post'),
     path('response', views.makeResponse, name='create_response'),
-    path('feed', views.getFeed, name='get_feed')
+    path('feed', views.getFeed, name='get_feed'),
+    path('drop/<int:post_id>/', views.getDrop, name='get_drop'),
+    path('total', views.getTotalDrops, name='get_total_drops'),
 ]
--- post/views.py
+++ post/views.py
@@ -10,7 +10,7 @@ from user.models import User
 def makePost(request):
     if request.method == "POST":
         json_data = json.loads(request.body)
-        user = authenticate(username=json_data['username'], password=json_data['password'])
+        user = User.objects.get(username=json_data['username'])
         if user is not None:
             author = User.objects.get(username=json_data['username'])
             post = Post(user=author, content=json_data['content'])
@@ -22,7 +22,7 @@ def makePost(request):
 def makeResponse(request):
     if request.method == "POST":
         json_data = json.loads(request.body)
-        user = authenticate(username=json_data['username'], password=json_data['password'])
+        user = User.objects.get(username=json_data['username'])
         if user is not None:
             author = User.objects.get(username=json_data['username'])
             post = Post.objects.get(id=json_data['post_id'])
@@ -34,8 +34,34 @@ def makeResponse(request):
 @csrf_exempt
 def getFeed(request):
     if request.method == "GET":
-        feed = Post.objects.all().values()
+        feed = Post.objects.all().order_by('-pub_data').values()
         for post in feed:
             responses = Response.objects.filter(post_id=post['id']).values()
+            post['username'] = User.objects.get(id=post['user_id']).username
             post['post_feed'] = list(responses)
         return JsonResponse({"feed": list(feed)})
+
+@csrf_exempt
+def getDrop(request, post_id):
+    print("GET DROP")
+    if request.method == "GET":
+        post = Post.objects.get(id=post_id)
+        print(post)
+        ret = {
+            "username": post.user.username,
+            "pub_data": post.pub_data,
+            "content": post.content,
+            "post_feed": []
+        }
+        responses = Response.objects.filter(post_id=post_id).values()
+        for response in responses:
+            response['username'] = User.objects.get(id=response['user_id']).username
+            ret['post_feed'].append(response)
+        print(responses)
+        return JsonResponse(ret)
+
+@csrf_exempt
+def getTotalDrops(request):
+    if request.method == "GET":
+        print(len(Post.objects.all()))
+        return JsonResponse({"total": len(Post.objects.all())})
